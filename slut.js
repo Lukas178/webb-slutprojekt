bf1773f927975d25b1ab5faf5a342895ef93a063
Lukas178 <lukas.bruce@skola.taby.se>
2025-05-18 12:58:15 +0200

    bildspel bla

---
+++ slut.js
@@ -0,0 +1,13 @@
+
+let slideIndex = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlides() {
+  slides.forEach((slide, index) => {
+    slide.style.display = index === slideIndex ? "block" : "none";
+  });
+  slideIndex = (slideIndex + 1) % slides.length;
+  setTimeout(showSlides, 5000); 
+}
+
+showSlides();
